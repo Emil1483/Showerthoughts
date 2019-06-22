523060d349e6e05b9f96c7a2d30db3146ee09b8c
Emil Djupvik <emil14833@gmail.com>
2019-06-22 19:40:14 +0200

    improoved textstyles

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shower Thoughts',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          title: TextStyle(
+            fontWeight: FontWeight.w400,
+          ),
+          subtitle: TextStyle(
+            color: Colors.white54,
+            fontSize: 14.0,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
       routes: {
         "/": (BuildContext context) => HomeRoute(),
       },
--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -1,32 +1,14 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 
 import '../ui_elements/joke_tile.dart';
 
 class HomeRoute extends StatelessWidget {
-  Future _fetchPage() async {
-    await Future.delayed(Duration(seconds: 1));
-    return List.generate(10, (index) {
-      return {
-        "joke": "Hi hungry, im dad",
-        'author': "dad",
-      };
-    });
-  }
-
-  Widget _buildPage(List<Map<String, dynamic>> data) {
-    return ListView(
-      shrinkWrap: true,
-      primary: false,
-      children: data.map((Map<String, dynamic> productInfo) {
-        return ListTile(
-          leading: Icon(Icons.shopping_cart),
-          title: Text(productInfo['name']),
-          subtitle: Text('price: ${productInfo['price']}USD'),
-        );
-      }).toList(),
-    );
+  Future _fetchJoke() async {
+    await Future.delayed(Duration());
+    return {
+      "joke": "Hi hungry, im dad",
+      'author': "dad",
+    };
   }
 
   @override
@@ -41,55 +23,24 @@ class HomeRoute extends StatelessWidget {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return JokeTile(
-                  joke: math.Random().nextDouble() > 0.5
-                      ? null
-                      : {
-                          "joke": "Hi hungry, im dad. And this is a really long showerthought. To be or not to be. That is the question. ",
+                return FutureBuilder(
+                  future: _fetchJoke(),
+                  builder: (context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return JokeTile(joke: null);
+                    } else {
+                      return JokeTile(
+                        joke: {
+                          "joke":
+                              "Hi hungry, im dad. And this is a really long showerthought. To be or not to be. That is the question. ",
                           'author': "dad",
                         },
+                      );
+                    }
+                  },
                 );
               },
             ),
-          ),
-        ],
-      ),
-    );
-
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          ListView.builder(
-            primary: false,
-            itemBuilder: (BuildContext context, int index) {
-              return FutureBuilder(
-                future: _fetchPage(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState != ConnectionState.done) {
-                    return Container(
-                      height: MediaQuery.of(context).size.height * 3,
-                      alignment: Alignment.topCenter,
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  if (snapshot.hasError) {
-                    return Text("Error: ${snapshot.error}");
-                  }
-
-                  List<Map<String, dynamic>> data = snapshot.data;
-                  return ListView(
-                    shrinkWrap: true,
-                    primary: false,
-                    children: data.map((Map<String, dynamic> joke) {
-                      return ListTile(
-                        title: Text(joke['joke']),
-                        subtitle: Text(joke["author"]),
-                      );
-                    }).toList(),
-                  );
-                },
-              );
-            },
           ),
         ],
       ),
